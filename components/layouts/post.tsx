a4faffded616d345eb567f9f9ba8a418170e35bb
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-10-25 02:12:49 +0900

    🛠 Upgrade packages to latest & add antd

--- components/layouts/post.tsx
+++ components/layouts/post.tsx
@@ -1,28 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import Details from '../../utils/details';
 
 interface Props {
   children: JSX.Element;
   pathname: string;
 }
-
-const Background = styled.div`
-  background: #221919;
-  color: white;
-`;
-
-const Container = styled.div`
-  margin: 0 auto;
-  max-width: 700px;
-  padding: 20px 15px;
-  width: 100%;
-`;
-
-const Content = styled.div`
-  font-size: 24px;
-  line-height: 43px;
-`;
 
 const PostLayout = ({ children, pathname }: Props): JSX.Element => {
   const [details, setDetails] = useState<Details>(new Details('', ''));
@@ -33,20 +15,18 @@ const PostLayout = ({ children, pathname }: Props): JSX.Element => {
   }, []);
   const { title, author } = details;
   return (
-    <Background>
-      <Container>
-        <div>Title: {title}</div>
-        <div>Author: {author}</div>
-        <Content>{children}</Content>
-        <style>
-          {`
-            img {
-              max-width: 100%;
-            }
+    <div>
+      <div>Title: {title}</div>
+      <div>Author: {author}</div>
+      {children}
+      <style>
+        {`
+          img {
+            max-width: 100%;
+          }
           `}
-        </style>
-      </Container>
-    </Background>
+      </style>
+    </div>
   );
 };
 
