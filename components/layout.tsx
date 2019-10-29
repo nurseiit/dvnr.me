30d61b2ca2d625839340805542bf7d4fe4809b75
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-10-29 23:40:35 +0900

    🏗 Add footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -36,6 +36,53 @@ const Header = (): JSX.Element => {
   );
 };
 
+const Footer = (): JSX.Element => (
+  <Flex align="center" justify="space-between" my="15px">
+    <Box>
+      <ChLink
+        style={{ fontWeight: 400, textDecoration: 'underline' }}
+        color="teal.500"
+        href="mailto:nurs@unist.ac.kr"
+      >
+        mail
+      </ChLink>{' '}
+      &bull;{' '}
+      <ChLink
+        style={{ fontWeight: 400, textDecoration: 'underline' }}
+        color="teal.500"
+        href="https://www.linkedin.com/in/nurseiit"
+      >
+        linkedin
+      </ChLink>{' '}
+      &bull;{' '}
+      <ChLink
+        style={{ fontWeight: 400, textDecoration: 'underline' }}
+        color="teal.500"
+        href="https://twitter.com/a.nurseiit"
+      >
+        twitter
+      </ChLink>{' '}
+      &bull;{' '}
+      <ChLink
+        style={{ fontWeight: 400, textDecoration: 'underline' }}
+        color="teal.500"
+        href="https://github.com/nurseiit"
+      >
+        github
+      </ChLink>
+    </Box>
+    <Box>
+      <ChLink
+        style={{ fontWeight: 400, textDecoration: 'underline' }}
+        color="teal.500"
+        href="https://github.com/nurseiit/inspire"
+      >
+        edit this page
+      </ChLink>
+    </Box>
+  </Flex>
+);
+
 interface Props {
   children: JSX.Element | string | null;
 }
@@ -45,6 +92,7 @@ const Layout = ({ children }: Props): JSX.Element => (
     <Box mx="20px">
       <Header />
       {children}
+      <Footer />
     </Box>
   </Box>
 );
