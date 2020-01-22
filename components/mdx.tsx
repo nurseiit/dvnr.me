92a1a2e89e1343c032cb30d7e3bbef2de103aafa
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-01-23 01:45:18 +0600

    ➕ Add/configure styled-components dependency.
    Also update MDXProvider components.

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,24 +1,58 @@
 /* eslint react/jsx-props-no-spreading: 0 */
-import React from 'react';
-import { Link } from '@chakra-ui/core';
+import styled, { css } from 'styled-components';
 
-const Wrapper = ({ ...props }): JSX.Element => <div {...props} />;
-const img = ({ ...props }): JSX.Element => <div {...props} />;
-const pre = ({ ...props }): JSX.Element => <div {...props} />;
-const code = ({ ...props }): JSX.Element => <div {...props} />;
-const h1 = ({ ...props }): JSX.Element => (
-  <div style={{ fontSize: '2em' }} {...props} />
-);
-const h2 = ({ ...props }): JSX.Element => (
-  <div style={{ fontSize: '1.8em' }} {...props} />
-);
-const p = ({ ...props }): JSX.Element => <div {...props} />;
-const ul = ({ ...props }): JSX.Element => <div {...props} />;
-const ol = ({ ...props }): JSX.Element => <div {...props} />;
-
+const Wrapper = styled.main`
+  margin: 0 auto;
+  padding: 0 0 10px 0;
+  font-size: 0.95rem;
+`;
+const a = styled.a`
+  text-decoration: underline;
+`;
+const img = styled.img`
+  max-width: 100%;
+`;
+const pre = styled.pre`
+  overflow: scroll;
+`;
+const code = styled.code`
+  display: block;
+  padding: 10px;
+  font-size: 1.6rem;
+  border: 1px solid grey;
+  border-radius: 3px;
+`;
+const h = css`
+  font-weight: 500;
+  letter-spacing: -0.0125rem;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+const h1 = styled.h1`
+  ${h}
+  font-size: 1.8rem;
+`;
+const h2 = styled.h2`
+  ${h}
+  font-size: 1.4rem;
+`;
+const p = styled.p`
+  margin: 10px 0;
+`;
+const list = css`
+  margin: 0;
+  padding: 0;
+  list-style-position: inside;
+`;
+const ul = styled.ul`
+  ${list}
+`;
+const ol = styled.ol`
+  ${list}
+`;
 export default {
   wrapper: Wrapper,
-  a: Link,
+  a,
   img,
   pre,
   code,
