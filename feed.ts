b4bc07cada464dc2e4a793da23bdc0715bc8a399
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-02-15 18:31:38 +0600

    🚧 Start migrating old posts.

--- feed.ts
+++ feed.ts
@@ -1,24 +1,26 @@
 export default [
   {
-    id: 'example-post',
-    title: 'Static blog generator using Next.js, Styled Components, MDX',
-    description:
-      "Modern frontend era gave us tons of nice JavaScript tools and libraries like Babel, React, Webpack. Ecosystems give almost infinite possibilities in the hands of developers. Let's create a custom fully functional static site generator with modern JavaScript.",
-    tags: [
-      'JavaScript',
-      'TypeScript',
-      'Next.js',
-      'Blog',
-      'MDX',
-      'Babel',
-      'SSR',
-      'markdown',
-      'generator',
-      'components',
-      'nodejs',
-      'web'
-    ],
-    createdAt: '2019-03-13',
+    id: 'getting-out-of-the-comfort-zone',
+    title: 'Getting Out Of The Comfort Zone',
+    description: 'My thoughts on performing well at various exams.',
+    tags: ['personal', 'olympiads', 'exams', 'en'],
+    createdAt: '2016-12-24',
+    public: true
+  },
+  {
+    id: 'i-am-back-again',
+    title: "I'm Back... (again)",
+    description: 'My Personal View on Augmented Reality.',
+    tags: ['personal', 'thoughts', 'ru'],
+    createdAt: '2016-09-12',
+    public: true
+  },
+  {
+    id: 'trying-to-do-my-best',
+    title: 'Trying To Do My Best',
+    description: 'It’s been a long time since I last wrote here (2 months).',
+    tags: ['personal', 'olympiads', 'en'],
+    createdAt: '2016-03-09',
     public: true
   }
 ];
