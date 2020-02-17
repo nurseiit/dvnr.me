7adf050f6f310246ff578f1c97ef8a2e5e8a1a2e
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-02-17 16:51:16 +0600

    🎨 Adjust Images on sbornik post.

--- components/posts/no-more-sbornik.tsx
+++ components/posts/no-more-sbornik.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import Carousel from 'react-images';
+import Gallery from 'react-grid-gallery';
 
 const images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(val => ({
-  src: `/img/no-more-sbornik/${val}.jpg`
+  src: `/img/no-more-sbornik/${val}.jpg`,
+  thumbnail: `/img/no-more-sbornik/${val}.jpg`,
+  thumbnailWidth: 0,
+  thumbnailHeight: 0
 }));
 
-const PhotoCarousel = (): JSX.Element => <Carousel views={images} />;
+const PhotoCarousel = (): JSX.Element => (
+  <div
+    style={{
+      display: 'block',
+      minHeight: '1px',
+      width: '100%',
+      overflow: 'auto',
+      textAlign: 'center'
+    }}
+  >
+    <Gallery images={images} enableImageSelection={false} />
+  </div>
+);
 
 export default PhotoCarousel;
