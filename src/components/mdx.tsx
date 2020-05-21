a48887eaa2676116c389144c49aef18e1495341d
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-05-21 20:12:42 +0600

    🐛 Fix img width & lists padding

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,18 +1,28 @@
-/* eslint react/jsx-props-no-spreading: 0 */
 import styled from 'styled-components';
 import Code from './code';
 
 const img = styled.img`
+  min-width: 125%;
   max-width: 125%;
   margin-left: -12.5%;
-  @media (max-width: 825px) {
+  @media (max-width: 875px) {
     max-width: 100%;
+    min-width: 100%;
     margin-left: 0;
   }
   margin-bottom: 1.3em;
 `;
 
+const ul = styled.ul`
+  padding-left: 30px;
+`;
+const ol = styled.ol`
+  padding-left: 30px;
+`;
+
 export default {
   img,
+  ol,
+  ul,
   code: Code,
 };
