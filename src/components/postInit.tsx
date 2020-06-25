f6318b848a08be0951160fb176545ecbaec6001a
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-06-25 22:29:21 +0600

    🔍 Add per page configuration for posts

--- src/components/postInit.tsx
+++ src/components/postInit.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
 import { format } from 'date-fns';
+import { BlogJsonLd } from 'next-seo';
 
 import { findPost } from '../utils/posts';
 import { rhythm } from '../utils/typography';
 
 interface Props {
   id: string;
+  imagePath?: string;
 }
 
-const PostInit = ({ id }: Props): JSX.Element => {
+const PostInit = ({ id, imagePath }: Props): JSX.Element => {
   const post = findPost(id);
   return (
     <>
@@ -21,6 +23,15 @@ const PostInit = ({ id }: Props): JSX.Element => {
         {format(post.createdAt, 'MMMM d, yyyy')}
       </span>
       <p />
+      <BlogJsonLd
+        url={`https://devnur.me/posts/${post.id}`}
+        title={post.title}
+        images={imagePath ? [`https://devnur.me${imagePath}`] : undefined}
+        datePublished={post.createdAt.toISOString()}
+        dateModified={post.updatedAt.toISOString()}
+        authorName="Nurseiit A."
+        description={post.description}
+      />
     </>
   );
 };
