dcdc215c06b73b76b5f7c63a5625e740258ce8bf
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-06-25 22:09:26 +0600

    🔍 Add default website SEO

---
+++ src/next-seo.config.ts
@@ -0,0 +1,12 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_UK',
+    // eslint-disable-next-line
+    site_name: '/dev/nur – inspirations by Nurseiit A.',
+  },
+  twitter: {
+    handle: '@anurseiit',
+    cardType: 'summary_large_image',
+  },
+};
