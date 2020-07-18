93c760b6f1c15bb6722b51f0d14cbb7e222840fc
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-07-18 23:14:05 +0600

    🔧 Configure jest coverage of untested files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>'],
   moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+  collectCoverageFrom: ['src/**/*.{js,ts,tsx}'],
   testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
   transform: {
